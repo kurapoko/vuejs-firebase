869d226a5bd58e8663f4de44420fc759e91224c6
m.kurahara <kuraharamasahiro@gmail.com>
2020-09-25 00:11:40 +0900

    Addressコンポーネントの作成

---
+++ src/components/Address.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>アドレスコンポーネント</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Address />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Address from '@/components/Address.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Address
   }
 }
 </script>
