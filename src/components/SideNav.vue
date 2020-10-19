8395aafb52445a11b01a7ff3d58b05bda8efe324
m.kurahara <kuraharamasahiro@gmail.com>
2020-10-20 01:00:59 +0900

    vuexを使ってドロワーを開閉できるようにする。

---
+++ src/components/SideNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container fluid fill-height>
+
+
+    <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>Kurahara Masahiro</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item v-for="(item, index) in items" :key="index">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { title: '連絡先一覧', icon: 'mdi-menu' }
+      ]
+    }
+  }
+}
+</script>
