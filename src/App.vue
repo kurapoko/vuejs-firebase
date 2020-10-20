3ac83fb054345411905166be9221d5cfc09af90a
m.kurahara <kuraharamasahiro@gmail.com>
2020-10-21 01:11:09 +0900

    ログイン機能の実装

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,7 @@
 </template>
 
 <script>
+import firebase from 'firebase';
 import { mapActions } from 'vuex';
 import SideNav from './components/SideNav';
 
@@ -29,12 +30,18 @@ export default {
   components: {
     SideNav
   },
-
+  created () {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setLoginUser(user)
+      }
+    })
+  },
   data: () => ({
     //
   }),
   methods: {
-    ...mapActions(['toggleSideMenu'])
+    ...mapActions(['toggleSideMenu', 'setLoginUser'])
   }
 };
 </script>
