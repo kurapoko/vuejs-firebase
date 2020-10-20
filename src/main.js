dc1e33bb5c756e5848201fd232a9297be920f805
m.kurahara <kuraharamasahiro@gmail.com>
2020-10-21 00:37:09 +0900

    firebaseの初期設定

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkYX5e6FE58DgcbsrJBtlj0BkrhiZUIX0",
+  authDomain: "my-address-pj-e521b.firebaseapp.com",
+  databaseURL: "https://my-address-pj-e521b.firebaseio.com",
+  projectId: "my-address-pj-e521b",
+  storageBucket: "my-address-pj-e521b.appspot.com",
+  messagingSenderId: "256024986187",
+  appId: "1:256024986187:web:c8d912ea749d5eb35a43ce",
+  measurementId: "G-B97R8WSD2P"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
